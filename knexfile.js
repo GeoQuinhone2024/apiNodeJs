bbdcbeef5e5959171c2c164a4682f3f502a46556
GeoQuinhone2024 <geovanequinhone2018@hotmail.com>
2025-06-14 14:28:52 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: 'eventosunialfa',
+      user: 'root',
+      password: ''
+    },
+    pool: { min: 2, max: 10 },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    }
+  }
+};
+
+
+
